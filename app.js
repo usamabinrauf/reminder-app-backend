eb15a35c2b242e9187bc9835564d21f90126de49
Usama Bin Rauf <110464832+usamabinrauf@users.noreply.github.com>
2023-09-08 17:11:03 +0500

    Updated email sending logic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
+const cron = require('node-cron');
+const admin = require('firebase-admin'); // Firebase Admin SDK
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +12,45 @@ app.use(cors());
 app.use(express.json());
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
+
+// Initialize Firebase Admin SDK (make sure to initialize it with your Firebase Admin credentials)
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+// Schedule email reminders using node-cron
+cron.schedule('* * * * *', async () => {
+    try {
+        const now = Date.now();
+        const deadlineTimeFrame = 60000; // 1 minute (adjust as needed)
+
+        // Query reminders where the deadline is within the specified time frame
+        const querySnapshot = await admin.firestore()
+            .collection('reminders')
+            .where('deadline', '>=', now)
+            .where('deadline', '<=', now + deadlineTimeFrame)
+            .get();
+
+        // Iterate through the query results and send email reminders
+        querySnapshot.forEach(async (doc) => {
+            const reminder = doc.data();
+
+            // Send email reminder using SendGrid
+            await sgMail.send({
+                to: reminder.email,
+                subject: `Reminder: ${reminder.title} Deadline`,
+                text: reminder.description,
+            });
+        });
+    } catch (error) {
+        console.error('Error sending scheduled email reminders:', error);
+    }
+});
 
 app.post('/send-email', async (req, res) => {
     const { to, subject, text } = req.body;
