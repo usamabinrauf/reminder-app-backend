6b39d7c9c320cd7eb9ce36f108dd532dd4d388ba
Usama Bin Rauf <110464832+usamabinrauf@users.noreply.github.com>
2023-09-08 14:23:22 +0500

    Modified email scheduling logic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const admin = require('firebase-admin');
 const sgMail = require('@sendgrid/mail');
+const axios = require('axios');
 const dotenv = require('dotenv');
 const schedule = require('node-schedule');
 
@@ -9,27 +9,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Initialize Firebase Admin SDK
-const serviceAccount = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escape characters
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -39,20 +18,9 @@ app.use(express.json());
 app.post('/add-reminder', async (req, res) => {
     const { to, subject, text, dueTime } = req.body;
 
-    // Validate the dueTime format
-    const parsedDueTime = new Date(dueTime);
-    if (isNaN(parsedDueTime)) {
-        return res.status(400).json({ error: 'Invalid dueTime format.' });
-    }
-
     try {
-        // Add a new document to the 'reminders' collection
-        await db.collection('reminders').add({
-            to,
-            subject,
-            text,
-            dueTime: parsedDueTime.toISOString(), // Convert to ISO format
-        });
+        // Schedule the reminder and simulate adding it to a database
+        await scheduleReminder(to, subject, text, dueTime);
 
         res.status(200).json({ message: 'Reminder added successfully.' });
     } catch (error) {
@@ -61,46 +29,47 @@ app.post('/add-reminder', async (req, res) => {
     }
 });
 
-// Schedule a recurring job to send reminders
-schedule.scheduleJob('0 * * * *', async () => {
-    try {
-        // Get due reminders from Firestore
-        const now = new Date();
-        const querySnapshot = await db
-            .collection('reminders')
-            .where('dueTime', '<=', now.toISOString()) // Compare in ISO format
-            .get();
+// Function to schedule a reminder
+async function scheduleReminder(to, subject, text, dueTime) {
+    const now = new Date();
+    const deadline = new Date(dueTime);
 
-        const dueReminders = [];
-        querySnapshot.forEach((doc) => {
-            const reminder = doc.data();
-            reminder.id = doc.id; // Include the document ID for future reference
-            dueReminders.push(reminder);
-        });
-
-        // Send emails and delete reminders
-        for (const reminder of dueReminders) {
-            const { to, subject, text, id } = reminder;
-
-            const emailData = {
-                to,
-                from: process.env.VERIFIED_EMAIL,
-                subject,
-                text,
-            };
-
-            // Use the promise returned by sgMail.send to handle success and error
-            await sgMail.send(emailData);
-
-            // Remove the sent reminder from Firestore
-            await db.collection('reminders').doc(id).delete();
-        }
-
-        console.log('Sent', dueReminders.length, 'reminders');
-    } catch (error) {
-        console.error('Error sending reminders:', error);
+    if (deadline <= now) {
+        console.log('The deadline has already passed.');
+        return;
     }
-});
+
+    // Calculate the delay for sending the reminder
+    const delay = deadline - now;
+
+    // Schedule a job to send the reminder email
+    schedule.scheduleJob(deadline, async () => {
+        const emailData = {
+            to,
+            from: process.env.VERIFIED_EMAIL,
+            subject,
+            text,
+        };
+
+        try {
+            // Simulate sending the email
+            await sendEmail(emailData);
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
+    });
+}
+
+// Function to simulate sending an email
+async function sendEmail(emailData) {
+    try {
+        // Simulate sending the email via a mock API
+        await axios.post('https://64d8e4865f9bf5b879cea997.mockapi.io/reminders', emailData);
+        console.log('Email sent successfully.');
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
